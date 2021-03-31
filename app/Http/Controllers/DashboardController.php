4d83fa6e2b1c8d5a8e4f5eed63ba9a70f7a0acde
owenwattimena <wentoxwtt@gmail.com>
2021-03-31 18:06:45 +0900

    dashboard card

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Kriteria;
 use App\Models\Alternatif;
 use App\Models\Subkriteria;
+use App\Models\TenagaMedis;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -14,6 +16,10 @@ class DashboardController extends Controller
         $data['alternatif'] = Alternatif::all();
         $data['kriteria'] = Kriteria::all();
         $data['subkriteria'] = Subkriteria::all();
+        $data['pukesmas'] = User::where('level', 'puskesmas')->get();
+        if (\Auth::user()->level == 'puskesmas') {   
+            $data['tenagaMedis'] = TenagaMedis::where('id_puskesmas', \Auth::user()->id)->get();
+        }
         return view('dashboard.index',$data);
 
     }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -10,73 +10,93 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-3 col-sm-12 col-xs-12">
-            <div class="board">
-                <div class="panel panel-primary">
-                    <div class="number">
-                        <h3>
-                            <h3>{{ count($alternatif) }}</h3>
-                            <small>Alternatif</small>
-                        </h3>
-                    </div>
-                    <div class="icon">
-                        <i class="fa fa fa-user fa-5x red"></i>
-                    </div>
+        @if (\Auth::user()->level == 'admin')
 
+            <div class="col-md-3 col-sm-12 col-xs-12">
+                <div class="board">
+                    <div class="panel panel-primary">
+                        <div class="number">
+                            <h3>
+                                <h3>{{ count($alternatif) }}</h3>
+                                <small>Alternatif</small>
+                            </h3>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa fa-user fa-5x red"></i>
+                        </div>
+
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-md-3 col-sm-12 col-xs-12">
-            <div class="board">
-                <div class="panel panel-primary">
-                    <div class="number">
-                        <h3>
-                            <h3>{{ count($kriteria) }}</h3>
-                            <small>Kriteria</small>
-                        </h3>
-                    </div>
-                    <div class="icon">
-                        <i class="fa fa-list fa-5x blue"></i>
-                    </div>
+            <div class="col-md-3 col-sm-12 col-xs-12">
+                <div class="board">
+                    <div class="panel panel-primary">
+                        <div class="number">
+                            <h3>
+                                <h3>{{ count($kriteria) }}</h3>
+                                <small>Kriteria</small>
+                            </h3>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-list fa-5x blue"></i>
+                        </div>
 
+                    </div>
                 </div>
             </div>
-        </div>
 
-        <div class="col-md-3 col-sm-12 col-xs-12">
-            <div class="board">
-                <div class="panel panel-primary">
-                    <div class="number">
-                        <h3>
-                            <h3>{{ count($subkriteria) }}</h3>
-                            <small>Sub Kriteria</small>
-                        </h3>
-                    </div>
-                    <div class="icon">
-                        <i class="fa fa-list fa-5x green"></i>
-                    </div>
+            <div class="col-md-3 col-sm-12 col-xs-12">
+                <div class="board">
+                    <div class="panel panel-primary">
+                        <div class="number">
+                            <h3>
+                                <h3>{{ count($subkriteria) }}</h3>
+                                <small>Sub Kriteria</small>
+                            </h3>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-list fa-5x green"></i>
+                        </div>
 
+                    </div>
                 </div>
             </div>
-        </div>
+        @elseif(\Auth::user()->level == 'dinkes' || \Auth::user()->level == 'gubernur')
+            <div class="col-md-3 col-sm-12 col-xs-12">
+                <div class="board">
+                    <div class="panel panel-primary">
+                        <div class="number">
+                            <h3>
+                                <h3>{{ count($pukesmas) }}</h3>
+                                <small>Puskesmas</small>
+                            </h3>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-hospital-o fa-5x yellow"></i>
+                        </div>
 
-        {{-- <div class="col-md-3 col-sm-12 col-xs-12">
-            <div class="board">
-                <div class="panel panel-primary">
-                    <div class="number">
-                        <h3>
-                            <h3>89,645</h3>
-                            <small>Daily Profits</small>
-                        </h3>
                     </div>
-                    <div class="icon">
-                        <i class="fa fa-user fa-5x yellow"></i>
-                    </div>
-
                 </div>
             </div>
-        </div> --}}
+        @else
+            <div class="col-md-3 col-sm-12 col-xs-12">
+                <div class="board">
+                    <div class="panel panel-primary">
+                        <div class="number">
+                            <h3>
+                                <h3>{{ count($tenagaMedis) }}</h3>
+                                <small>Tenaga Medis</small>
+                            </h3>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-hospital-o fa-5x yellow"></i>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        @endif
 
     </div>
 @endsection
